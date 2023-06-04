72feb904466c852c727325766ad231c8b2c11814
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-06-03 22:01:35 -0600

    Bueno esto es lo basico para la sorpresa para es niña linda

---
+++ JS/efectosE.js
@@ -0,0 +1,16 @@
+window.sr = ScrollReveal();
+    sr.reveal('.secciontop', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-500px'
+    });
+    sr.reveal('.palabrasBienvenid' ,{
+        duration:2500,
+        origin: 'left',
+        distance: '-600px'
+    });
+    sr.reveal('.nav' ,{
+        duration:2500,
+        origin: 'right',
+        distance: '-600px'
+    });
