4045348bd41463f92a4a608a67813d81e01c6a6f
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-06-04 21:01:43 -0600

    Amm ni se que hice pero bueno ahi va

--- JS/efectosE.js
+++ JS/efectosE.js
@@ -14,3 +14,23 @@ window.sr = ScrollReveal();
         origin: 'right',
         distance: '-600px'
     });
+    sr.reveal('.subtitle, .about__paragraph', {
+        duration: 2500,
+        origin: 'right',
+        distance: '-800px'
+    });
+    sr.reveal('.efecto_derecha ' ,{
+        duration:2500,
+        origin: 'right',
+        distance: '-600px'
+    });
+    sr.reveal('.efecto_bottom', {
+        duration: 2500,
+        origin: 'bottom',
+        distance: '-500px',
+    });
+    sr.reveal('.efecto_izquieda ' ,{
+        duration:2500,
+        origin: 'left',
+        distance: '-600px'
+    });
